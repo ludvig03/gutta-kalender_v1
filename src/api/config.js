e44e6125c4b6e180eb95222214b5ce4685c198f0
Ludvig <ludvig.bergsaker@gmail.com>
2022-10-16 16:54:01 +0200

    api link

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,1 @@
-export const API_URL = 'http://localhost:8080'
-export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2NTY3NTM0OX0.nc03a06OAyzwwlTAKu8V48fHFsYEeHZA-ArE0la0oNI'
+export const API_URL = 'https://gutta-kalender.herokuapp.com/'
